45b6db9df1bfce896bb8d2fca04cadab4c73301a
thatSameZohan <king31121996@gmail.com>
2025-09-21 13:31:07 +0300

    hometask38.1

--- hometask38/src/main/java/org/spring/web/controller/ShopController.java
+++ hometask38/src/main/java/org/spring/web/controller/ShopController.java
@@ -1,11 +1,13 @@
 package org.spring.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @RequestMapping("/shop")
 @Controller
 public class ShopController {
 
-    @RequestMapping("/orders")
+    @RequestMapping(value = "/orders", method = RequestMethod.POST)
     public String ordersPage() {
         return "home";
     }
